e4f7b84422694648eb24807d29e628337825bd4c
Viseiras <javi.vice2@gmail.com>
2022-11-10 12:50:11 +0100

    Constraint y Relative

--- app/src/main/java/com/example/edicingrfica/RelativeLayout.java
+++ app/src/main/java/com/example/edicingrfica/RelativeLayout.java
@@ -2,16 +2,13 @@ package com.example.edicingrfica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class RelativeLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_relative_layout);
     }
 }
